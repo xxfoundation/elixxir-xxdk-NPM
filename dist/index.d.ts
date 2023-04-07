0acc0fca4330b9317bb33f0590f48f41496fce7d
Matt <matt@xx.network>
2023-04-07 22:22:36 +0700

    Remove dist folder

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { XXDKUtils } from './types';
-import './wasm/wasm_exec';
-declare global {
-    interface Window extends XXDKUtils {
-    }
-}
-export declare const loadUtils: () => Promise<XXDKUtils>;
-export * from './types';
